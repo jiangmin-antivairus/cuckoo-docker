7d9d274881b772305933fd38bb8febb257dc18b0
xuruoyu <xuruoyu@jiangmin.com>
2021-11-16 07:50:54 +0000

    auto commit

--- make.sh
+++ make.sh
@@ -7,8 +7,8 @@ echo 'git_version:' $git_version
 echo $git_version > version
 
 # 制作docker镜像
-docker build -t cuckoo:$git_version .
+docker build --no-cache -t cuckoo:$git_version .
 docker tag cuckoo:$git_version cuckoo:latest
 
 # 导出docker镜像
-docker save -o cuckoo.tar cuckoo:$git_version
+# docker save -o cuckoo.tar cuckoo:$git_version
